0d8284ddf13b459bde15efd862b30cd91890fb4c
Alan Weide <alan.d.weide@gmail.com>
2020-07-29 16:03:28 -0400

    Data 2020-07-28

--- get-data.sh
+++ get-data.sh
@@ -12,12 +12,24 @@ curl https://coronavirus.ohio.gov/static/COVIDSummaryData.csv -o data/$today.csv
 if [ -f "data/$yesterday.csv" ]; then
 if [[ $(diff data/$today.csv data/$yesterday.csv) ]]; then
     echo "Got new data."
+    branch=$(git branch --show-current)
+    git checkout master
+    git add data/*
+    git commit -m "Data through $today"
+    git push origin master
+    git checkout $branch
 else
     echo "No new data. Deleting file for $today."
     rm data/$today.csv
 fi
 else
     echo "Got new data."
+    branch=$(git branch --show-current)
+    git checkout master
+    git add data/*
+    git commit -m "Data through $today"
+    git push origin master
+    git checkout $branch
 fi
 
 popd
