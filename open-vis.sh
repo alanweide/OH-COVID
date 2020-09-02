49008d7721431c33121d6d2d5bb3cea05a0a9f2e
Alan Weide <alan.d.weide@gmail.com>
2020-09-02 16:29:11 -0400

    Tried to add an option to open-vis script for one-time launches. Failed.

--- open-vis.sh
+++ open-vis.sh
@@ -1,13 +1,25 @@
 # Starts a simple python HTTP server, then opens safari with the visualization webpage.
 
 port=8888
-if [ ! -z "$1" ]
-then
-    echo $1
-    port=$1
-fi
+kill=0
+while getopts ":k" arg
+do
+  case $arg in
+    k) echo "got kill option";;
+    p) port=$OPTARG;;
+  esac
+done
+
 python -m SimpleHTTPServer $port &
-sleep 0.1
+sleep 0.2
 open http://localhost:$port
 
-echo "Type \"kill -9 $!\" to stop HTTP server"
+echo "kill = $kill"
+if [ $kill -eq 1 ]
+then
+    sleep 0.2
+    echo "Killing HTTP server..."
+    kill -9 $!
+else
+    echo "Type \"kill -9 $!\" (or \"kill -9 \$!\") to stop HTTP server"
+fi
