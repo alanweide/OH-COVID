d23576fd0005a3b2658ab251485e1f0729135e5a
Alan Weide <alan.d.weide@gmail.com>
2020-09-23 17:54:28 -0400

    get-data script now uses correct data location.

--- get-data.sh
+++ get-data.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+. ${HOME}/.zshrc
+
 # Keep track of today's date and yesterday for comparing files.
 today=$(date +%Y-%m-%d)
 yesterday=$(date -v -1d +%Y-%m-%d)
 
 # Navigate to folder containing the local versions of the data.
-pushd ${HOME}/git/OH-COVID
+pushd ${COVID_HOME}
 
 # Get COVIDSummaryData from Ohio website.
 curl https://coronavirus.ohio.gov/static/COVIDSummaryData.csv -o data/$today.csv
